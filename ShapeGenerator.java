12667105765929d5cccb4e2bbbcfbd3edbed084b
thangiann <thanogiannopoulos13@gmail.com>
2025-03-26 23:53:41 +0200

    new file

---
+++ ShapeGenerator.java
@@ -0,0 +1,15 @@
+public class ShapeGenerator {
+
+    private String[] types = {"Square", "Triangle", "Pentagon", "Circle"};
+    private 
+
+    public ShapeGenerator(int number){
+        
+        for (String type:types){
+            
+            for (int i = 0; i < 2; i++){
+
+            }
+        }
+    }
+}
