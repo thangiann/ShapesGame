4ffad715b30ca8fc6283f517f21fba091b7963aa
thangiann <thanogiannopoulos13@gmail.com>
2025-03-27 20:41:23 +0200

    changes

--- ShapeGenerator.java
+++ ShapeGenerator.java
@@ -1,15 +1,41 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ShapeGenerator{
 
-    private Shape[] shapes;
-    //private String[] types = {"Square", "Triangle", "Pentagon", "Circle"};
-    private int[] bounding_area_valus = {1,2,3,4,8,12,16};
+    private ArrayList<Shape> shapes;
+    private int[] bounding_area_values = {1,2,3,4,8,12,16};
+
+    Random random = new Random();
 
     public ShapeGenerator(int number){
         
-        shapes = new Shape[4 * number];
+        shapes = new ArrayList<>();
 
+        for (int i = 0; i < number; i++){
+            
+            int index = random.nextInt(bounding_area_values.length - 1);
+            
+            Square square = new Square(bounding_area_values[index]);
+            shapes.add(square);
+        }
+
+        for (int i = 0; i < number; i++){
+            
+            int index = random.nextInt(bounding_area_values.length - 1);
+            
+            Triangle triangle = new Triangle(bounding_area_values[index]);
+            shapes.add(triangle);
+        }
+
+        for (int i = 0; i < number; i++){
+            
+            int index = random.nextInt(bounding_area_values.length - 1);
+            int bonus = random.nextInt(11);
+
+            BonusCircle circle = new BonusCircle(bounding_area_values[index], bonus);
+            shapes.add(circle);
+        }
         
     }
     
